f8d40fa5b6611fe9e309799f8483e8bdc6f6789c
gongyusong1234 <gongyusong@vcinema.cn>
2021-10-28 21:18:15 +0800

    补齐丢失的参数

--- page/fristhome.py
+++ page/fristhome.py
@@ -58,4 +58,4 @@ class HomePage:
         return self.adc.get_element('HomePage', 'mymoviecollect')
     # 我的
     def my_home(self):
-        return self.adc.get_element('HomePage','my_home')
+        return self.adc.get_element('HomePage', 'my_home')
